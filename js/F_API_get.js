12cc385c55e6bd9d84746adf4a8d7ed108caa552
friaszavalzao3 <102121487+friaszavalzao3@users.noreply.github.com>
2022-10-01 13:11:34 -0600

    F) y G) Finalizado

---
+++ js/F_API_get.js
@@ -0,0 +1,13 @@
+fetch('https://reqres.in/api/users')//!Mandasmo como parametro la url de la api
+.then(resp =>resp.json()) //?convetrimos resp a tipo JSON y lo retornamos
+.then(respObj =>//? respObj es nuestro resp pero en tipo JSON
+    {
+        //*Accedemos a las colecciones que hay en nuestra api
+        console.log(respObj);
+        console.log(respObj.page);
+        console.log(respObj.per_page);
+        console.log(respObj.data);
+    }
+    );
+
+   
